84fbe42363fb77e4b85916e442dca7ab5a367152
Ko Sooyoul <kosooyoul@gmail.com>
2022-05-22 23:55:07 +0900

    🩹 Upt. second solution of Roman to Integer problem with Javascript

--- easy/roman-to-integer.js
+++ easy/roman-to-integer.js
@@ -2,53 +2,76 @@
  * @param {string} s
  * @return {number}
  */
- var romanToInt = function(s) {
-    // The first submission -> 232ms, 47.3MB
+// The first submission -> 224ms, 52.5MB
+var romanToInt = function(s) {
     var result = 0;
     
-    var i = s.length - 1;
-    do {
-        if (s[i] === "M") {
-            result += 1000;
-            if (s[i - 1] === "C") {
-                result -= 100;
-                --i;
-            }
-        } else if (s[i] === "D") {
-            result += 500;
-            if (s[i - 1] === "C") {
-                result -= 100;
-                --i;
-            }
-        } else if (s[i] === "C") {
-            result += 100;
-            if (s[i - 1] === "X") {
-                result -= 10;
-                --i;
-            }
-        } else if (s[i] === "L") {
-            result += 50;
-            if (s[i - 1] === "X") {
-                result -= 10;
-                --i;
-            }
-        } else if (s[i] === "X") {
-            result += 10;
-            if (s[i - 1] === "I") {
-                result -= 1;
-                --i;
-            }
-        } else if (s[i] === "V") {
-            result += 5;
-            if (s[i - 1] === "I") {
-                result -= 1;
-                --i;
-            }
-        } else if (s[i] === "I") {
-            result += 1;
-        }
-        
-    } while(--i >= 0);
+    var i = 0;
+    
+    while (s[i] === "M") {
+        result += 1000;
+        i++;
+    }
+
+    if (s[i] === "C" && s[i + 1] === "M") {
+        result += 900;
+        i += 2;
+    }
+    
+    while (s[i] === "D") {
+        result += 500;
+        i++;
+    }
+
+    if (s[i] === "C" && s[i + 1] === "D") {
+        result += 400;
+        i += 2;
+    }
+    
+    while (s[i] === "C") {
+        result += 100;
+        i++;
+    }
+    
+    if (s[i] === "X" && s[i + 1] === "C") {
+        result += 90;
+        i += 2;
+    }
+    
+    while (s[i] === "L") {
+        result += 50;
+        i++;
+    }
+
+    if (s[i] === "X" && s[i + 1] === "L") {
+        result += 40;
+        i += 2;
+    }
+    
+    while (s[i] === "X") {
+        result += 10;
+        i++;
+    }
+
+    if (s[i] === "I" && s[i + 1] === "X") {
+        result += 9;
+        i += 2;
+    }
+    
+    while (s[i] === "V") {
+        result += 5;
+        i++;
+    }
+
+    if (s[i] === "I" && s[i + 1] === "V") {
+        result += 4;
+        i += 2;
+    }
+
+    while (s[i] === "I") {
+        result += 1;
+        i++;
+    }
 
     return result;
 };
