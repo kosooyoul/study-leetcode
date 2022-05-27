2b0fd455baa4deaca9865ba86f4356453e67bc9d
Ko Sooyoul <kosooyoul@gmail.com>
2022-05-27 22:52:52 +0900

    ⚗️ Add. solution of longest common prefix problem with Javascript

---
+++ easy/longest-common-prefix.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+ var longestCommonPrefix = function(strs) {
+    // the first submission -> 72ms, 43.1mb
+    const len = strs.length;
+    
+    if (len == 0) {
+        return "";
+    }
+    
+    if (len == 1) {
+        return strs[0];
+    }
+    
+    const max = strs[0].length;
+    
+    let i, c;
+    for (i = 0; i < max; i++) {
+        c = strs[0][i];
+        for (j = 1; j < len; j++) {
+            if (c != strs[j][i]) {
+                return strs[0].substr(0, i);
+            }
+        }
+    }
+    
+    return strs[0];
+};
