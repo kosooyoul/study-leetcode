c55761124977e09785775ffb5aba6be86dc40a34
Ko Sooyoul <kosooyoul@gmail.com>
2022-06-08 17:13:37 +0900

    ⚡ Upt. third solution of Pascals Triangles problem with Java

--- easy/pascals-triangle.java
+++ easy/pascals-triangle.java
@@ -1,4 +1,5 @@
 // The second submission -> 1ms, 42.3mb
+/*
 class Solution {
     public List<List<Integer>> generate(int numRows) {
         List<List<Integer>> rows = new ArrayList<List<Integer>>();
@@ -16,6 +17,32 @@ class Solution {
                 newRow.add(row.get(j - 1) + row.get(j));
             }
             newRow.add(row.get(i));
+
+            row = newRow;
+            rows.add(row);
+        }
+        
+        return rows;
+    }
+}
+*/
+
+// The third submission -> 0ms, 41.4mb
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> rows = new ArrayList<List<Integer>>(numRows);
+        List<Integer> row = new ArrayList<Integer>();
+        row.add(1);
+        
+        rows.add(row);
+        
+        for (int i = 1; i < numRows; i++) {
+            List<Integer> newRow = new ArrayList<Integer>(i + 1);
+            newRow.add(1);
+            for (int j = 1; j < i; j++) {
+                newRow.add(row.get(j - 1) + row.get(j));
+            }
+            newRow.add(1);
 
             row = newRow;
             rows.add(row);
