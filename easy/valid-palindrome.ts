5800aa3d1228bad70220148c16e1ab53e54dc3c2
Ko Sooyoul <kosooyoul@gmail.com>
2022-06-09 23:57:03 +0900

    ⚗️ Add. first solution of Valid Palindrome problem with Typescript

---
+++ easy/valid-palindrome.ts
@@ -0,0 +1,21 @@
+// The first submission -> 127ms, 45.7mb
+function isPalindrome(s: string): boolean {
+    let front = 0;
+    let rear = s.length - 1;
+    
+    while (front < rear) {
+        while ((s[front] < 'a' || s[front] > 'z') && (s[front] < 'A' || s[front] > 'Z') && (s[front] < '0' || s[front] > '9')) front++;
+        while ((s[rear] < 'a' || s[rear] > 'z') && (s[rear] < 'A' || s[rear] > 'Z') && (s[rear] < '0' || s[rear] > '9')) rear--;
+        
+        if (front < rear) {
+            if (s[front].toLowerCase() != s[rear].toLowerCase()) {
+                return false;
+            }
+        }
+        
+        front++;
+        rear--;
+    };
+    
+    return true;
+};
