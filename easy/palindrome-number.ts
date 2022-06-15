37e178c16c71d7f5b2033704933ca13d88ebf3e7
Ko Sooyoul <kosooyoul@gmail.com>
2022-06-15 23:49:33 +0900

    🩹 Upt. unite solutions of palindrome number problem with Typescript

--- easy/palindrome-number.ts
+++ easy/palindrome-number.ts
@@ -1,5 +1,6 @@
+// The first submission -> 189ms, 51mb
+/*
 function isPalindrome(x: number): boolean {
-    // 189ms, 51mb
     if (x < 0) return false;
 
     let rev = 0;
@@ -8,4 +9,44 @@ function isPalindrome(x: number): boolean {
     }
 
     return rev === x;
+};
+*/
+
+// The second submission -> 127ms, 45.7mb
+/*
+function isPalindrome(s: string): boolean {
+    let front = 0;
+    let rear = s.length - 1;
+    
+    while (front < rear) {
+        while ((s[front] < 'a' || s[front] > 'z') && (s[front] < 'A' || s[front] > 'Z') && (s[front] < '0' || s[front] > '9')) front++;
+        while ((s[rear] < 'a' || s[rear] > 'z') && (s[rear] < 'A' || s[rear] > 'Z') && (s[rear] < '0' || s[rear] > '9')) rear--;
+        
+        if (front < rear) {
+            if (s[front].toLowerCase() != s[rear].toLowerCase()) {
+                return false;
+            }
+        }
+        
+        front++;
+        rear--;
+    };
+    
+    return true;
+};
+*/
+
+// The third submission -> 112ms, 45.0mb
+function isPalindrome(s: string): boolean {
+    const str = s.toLowerCase().replace(/[^a-z0-9]/g, "");
+    let front = 0;
+    let rear = str.length - 1;
+    
+    while (front < rear) {
+        if (str[front++] != str[rear--]) {
+            return false;
+        }
+    };
+    
+    return true;
 };
