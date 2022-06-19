00b8bdc36f45f469133cc02a7b7f82a9955cb3aa
Ko Sooyoul <kosooyoul@gmail.com>
2022-06-19 09:52:58 +0900

    ⚡️ Upt. second submission of happy number problem with Java

--- easy/happy-number.java
+++ easy/happy-number.java
@@ -1,4 +1,5 @@
 // The first submission => 5ms, 42.6mb
+/*
 class Solution {
     public boolean isHappy(int n) {
         Set<Integer> nums = new HashSet<Integer>();
@@ -19,6 +20,32 @@ class Solution {
         for (int i = 0; i < nums.length; i++) {
             num = Integer.parseInt(String.valueOf(nums[i]));
             sum = sum + num * num;
+        }
+        return sum;
+    }
+}*/
+
+// The second submission => 1ms, 39.9mb
+class Solution {
+    public boolean isHappy(int n) {
+        Set<Integer> nums = new HashSet<Integer>();
+        while(n != 1) {
+            nums.add(n);
+            n = this.nextNum(n);
+            if (nums.contains(n)) {
+                break;
+            }
+        }
+        return n == 1;
+    }
+    
+    private int nextNum(int n) {
+        int sum = 0;
+        int num;
+        while(n > 0) {
+            num = n % 10;
+            sum = sum + num * num;
+            n = n / 10;
         }
         return sum;
     }
