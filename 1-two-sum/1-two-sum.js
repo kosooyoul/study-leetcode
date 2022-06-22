85e56ac84ba4833e6ba4ffbf800b2c9e528b1daa
Ko Sooyoul <kosooyoul@gmail.com>
2022-06-22 18:55:26 +0900

    Time: 110 ms (57.74%), Space: 42.9 MB (39.15%) - LeetHub

---
+++ 1-two-sum/1-two-sum.js
@@ -0,0 +1,42 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    // The first submission
+    /*
+    for (var i = 0; i < nums.length; i++) {
+        for (var j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] == target) {
+                return [i, j]
+            }
+        }
+    }
+    */
+    
+    // The second submission
+    /*
+    let v
+    for (var i = 0; i < nums.length; i++) {
+        v = nums.indexOf(target - nums[i], i + 1)
+        if (v != -1) {
+            return [i, v] 
+        }
+    }
+    */
+    
+    // The third submissio
+    // REF: https://leetcode.com/problems/two-sum/discuss/2018256/1.Two-Sum-JS
+    let v = {};
+    let num
+    for(let i = 0; i < nums.length; i++) {
+        num = nums[i]
+        if (target - num in v) {
+            return [v[target - num], i]
+        } else {
+            v[num] = i
+        }
+    }
+    return []
+};
